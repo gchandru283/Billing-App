8cef19fa40792e2b0ec16a13a97cdb090ee4e31d
“gchandru283” <gchandru283@gmail.com>
2023-11-05 23:10:51 +0530

    Commit 2

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Header() {
   return (
     <div className='h-20 bg-white px-10 w-screen fixed shadow-md flex justify-center'>
-        <div className='flex items-center gap-5'><img src='/coffee-cup.jpg' className='h-20 p-1'/>
+        <div className='flex items-center gap-5'><img src='/coffee-cup.jpg' alt='coffee cup image' className='h-20 p-1'/>
         <div className='font-HarryP font-medium md:text-5xl text-3xl pt-2'>Sarmitha's Cafe </div></div>
 
     </div>
