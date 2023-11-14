d0c15c2d7f2d56d6d460ca7e730f67eedd0719f4
“gchandru283” <gchandru283@gmail.com>
2023-11-14 10:41:26 +0530

    Commit 8

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
+import Modal from "react-modal";
 import { HotCoffee, Snacks, ColdCoffee, Dessert } from "./components/Data";
 import Header from "./components/Header";
+import New from "./components/Preview";
 
-function Billing({ customer }) {
+function Billing() {
   const [selected, setSelected] = useState([]);
   const [totalCost, setTotalCost] = useState(0);
 
@@ -25,7 +27,6 @@ function Billing({ customer }) {
       ...customerDetails,
       [name]: value,
     });
-    console.log(customerDetails);
   };
 
   const handleChange = (e) => {
@@ -54,6 +55,54 @@ function Billing({ customer }) {
   const calculateTotalCost = (item) => {
     return item.cost * item.count;
   };
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const [inputValues, setInputValues] = useState({
+    name: "",
+    mobile: "",
+  });
+  const handleClear = () => {
+    setCustomerDetails({ name: "", mobile: "" });
+    setInputValues({ name: "", mobile: "" });
+    setSelected([]);
+    setTotalCost(0);
+  };
+  //-------------------------------------------------------------------------------------------------
+
+  const [jsonData, setJsonData] = useState(null);
+  const convertToJSON = () => {
+    const jsonItems = selected.map((item) => ({
+      Item: item.name,
+      Count: item.count,
+      Price: item.cost.toFixed(2),
+      Amount: calculateTotalCost(item).toFixed(2),
+    }));
+    const jsonCustomer = {
+      Name: customerDetails.name,
+      Mobile: customerDetails.mobile,
+    };
+    const jsonTotal = {
+      Total: totalCost.toFixed(2),
+    };
+
+    const newJsonData = {
+      Customer: jsonCustomer,
+      Items: jsonItems,
+      Total: jsonTotal,
+    };
+
+    setJsonData(newJsonData);
+  };
+
+  //--------------------------------------------------------------------------------------------------
 
   const renderSection = (sectionName, items) => (
     <div className="flex flex-col pb-4" key={sectionName}>
@@ -67,6 +116,9 @@ function Billing({ customer }) {
                 type="checkbox"
                 value={item.name}
                 id={item.id}
+                checked={selected.some(
+                  (selectedItem) => selectedItem.id === item.id
+                )}
                 onChange={(e) => handleChange(e)}
               />
               {item.name}
@@ -76,25 +128,6 @@ function Billing({ customer }) {
       </div>
     </div>
   );
-
-  const convertToJSON = () => {
-    const jsonItems = selected.map((item) => ({
-      Item: item.name,
-      Count: item.count,
-      Price: calculateTotalCost(item).toFixed(2),
-    }));
-
-    const jsonTotal = {
-      Total: totalCost.toFixed(2),
-    };
-
-    const jsonData = {
-      Items: jsonItems,
-      Total: jsonTotal,
-    };
-    console.log(customer);
-    console.log(JSON.stringify(jsonData, null, 2));
-  };
 
   return (
     <>
@@ -112,47 +145,51 @@ function Billing({ customer }) {
           <div className="text-xl md:text-2xl font-semibold font-glacial border-b-2 border-black px-2">
             Customer Bill
           </div>
-          <div className="flex justify-between w-11/12 max-w-3xl font-glacial px-2">
-            {" "}
-            <div className=" flex justify-between items-end">
-              <label htmlFor="name" className="text-lg md:text-xl mr-3">
+          <div className="flex justify-between md:flex-row flex-col gap-4 md:gap-0 w-11/12 max-w-3xl font-glacial px-2">
+            <div className="flex justify-between items-end">
+              <label htmlFor="name" className="text-lg md:text-xl md:mr-3">
                 Name :{" "}
               </label>
               <input
-                className="border-b border-black focus:outline-none bg-transparent text-lg px-3"
+                className="border-b border-black focus:outline-none bg-transparent text-lg md:px-3"
                 id="name"
                 name="name"
                 onChange={handleInput}
+                value={customerDetails.name}
                 required
                 type="text"
               />
             </div>
             <div className=" flex justify-between items-end">
-              <label htmlFor="mobile" className="text-lg md:text-xl mr-3 ">
+              <label htmlFor="mobile" className="text-lg md:text-xl md:mr-3 ">
                 Mobile Number :{" "}
               </label>
               <input
-                className="border-b border-black focus:outline-none bg-transparent text-lg px-3"
+                className="border-b border-black focus:outline-none bg-transparent text-lg md:px-3"
                 id="mobile"
                 name="mobile"
                 onChange={handleInput}
+                value={customerDetails.mobile}
                 required
                 type="text"
               />
             </div>
           </div>
-          <div className="md:h-80 overflow-y-auto scrollbar w-11/12 max-w-3xl px-2 md:px-10 border border-dashed rounded-sm md:rounded py-5 border-gray-300">
-            <table className="table table-fixed w-full text-lg md:text-xl font-NewsCycle tracking-wide">
+          <div className="md:h-80 overflow-y-auto scrollbar w-11/12 max-w-3xl px-1 md:px-10 border border-dashed rounded-sm md:rounded py-5 border-gray-300">
+            <table className="table table-fixed w-full md:text-xl font-NewsCycle tracking-wide">
               <thead>
                 <tr>
-                  <th className="w-2/5 text-left pl-8 md:pl-16 font-semibold border-b-2 border-gray-400">
+                  <th className="w-1/3 pb-2 text-left pl-7 md:pl-12 font-semibold border-b-2 border-gray-400">
                     Item
                   </th>
-                  <th className="w-1/4 pb-2 font-semibold border-b-2 border-gray-400">
+                  <th className="w-1/5 pb-2 font-semibold border-b-2 border-gray-400">
                     Count
                   </th>
-                  <th className="w-1/4 text-right pb-2 pr-8 font-semibold border-b-2 border-gray-400">
+                  <th className="w-1/5 text-right md:pr-8 pr-6 pb-2  font-semibold border-b-2 border-gray-400">
                     Price
+                  </th>
+                  <th className="w-1/4 text-right pb-2 pr-5 font-semibold border-b-2 border-gray-400">
+                    Amount
                   </th>
                 </tr>
               </thead>
@@ -160,18 +197,21 @@ function Billing({ customer }) {
                 {selected.map((item, id) => {
                   return (
                     <tr key={id}>
-                      <td className="md:pl-10 pl-5 p-0.5 ">{item.name}</td>
+                      <td className=" pl-3 p-0.5 ">{item.name}</td>
                       <td className="text-center p-0.5">
                         <input
                           onFocus={null}
-                          className="w-16 text-center focus:outline-none bg-transparent text-lg"
+                          className="w-16 text-center focus:outline-none bg-transparent"
                           type="number"
                           id={item.id}
                           value={item.count}
                           onChange={(e) => handleCountChange(e, item.id)}
                         />
                       </td>
-                      <td className="text-right mx-auto pr-8 p-0.5 text-lg">
+                      <td className="text-right mx-auto pr-6 p-0.5">
+                        {item.cost.toFixed(2)}
+                      </td>
+                      <td className="text-right mx-auto pr-5 p-0.5">
                         {calculateTotalCost(item).toFixed(2)}
                       </td>
                     </tr>
@@ -180,21 +220,40 @@ function Billing({ customer }) {
               </tbody>
             </table>
           </div>
-          <div className="flex items-center justify-around gap-9 w-full mt-4 max-w-3xl px-5 sm:px-10 ">
+          <div className="flex flex-col md:flex-row items-center justify-around gap-9 w-full md:mt-4 max-w-3xl px-5 sm:px-10 ">
             <div className="font-semibold md:text-xl text-lg w-48 md:w-52">
               Total Cost: Rs.{totalCost.toFixed(2)}
             </div>
-            <div>
+            <div className="flex gap-5">
+              <input
+                type="button"
+                className="bg-red-600 hover:scale-100 scale-95 md:scale-100 md:hover:scale-105 transform duration-300 cursor-pointer text-white p-1 px-3 text-lg rounded font-NewsCycle tracking-wide"
+                value="Clear"
+                onClick={handleClear}
+              />
               <input
                 type="button"
                 className="bg-gray-600 hover:scale-100 scale-95 md:scale-100 md:hover:scale-105 transform duration-300 cursor-pointer text-white p-1 px-3 text-lg rounded font-NewsCycle tracking-wide"
                 value="Print Receipt"
-                onClick={convertToJSON}
+                onClick={() => {
+                  convertToJSON();
+                  openModal();
+                }}
               />
             </div>
           </div>
         </div>
       </div>
+
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={closeModal}
+        contentLabel="New Component Modal"
+        className="w-fit m-auto modalScrollbar bg-yellow-100 overflow-auto h-5/6 top-1/2 md:top-1/2 md:left-1/2 left-10 transform md:-translate-x-1/2 -translate-x-5 -translate-y-1/2 rounded border absolute p-3"
+        ariaHideApp={false}
+      >
+        <New jsonData={jsonData} />
+      </Modal>
     </>
   );
 }
--- src/components/Data.js
+++ src/components/Data.js
@@ -1,41 +1,41 @@
 export const HotCoffee = [
     { name : "Cappuccino", cost : 90, id : 11  },
     { name : "Cafe Latte", cost : 90, id : 12  },
-    { name : "Velvet Coffee", cost : 280, id : 13  },
-    { name : "Flat White", cost : 240, id : 14  },
-    { name : "Cinnamon Coffee", cost : 190, id : 15  },
-    { name : "Espresso", cost :140, id : 16  },
-    { name : "Vannila Latte", cost : 190, id : 17  },
-    { name : "Filter Coffee", cost : 290, id : 18  },
+    { name : "Velvet Coffee", cost : 150, id : 13  },
+    { name : "Flat White", cost : 80, id : 14  },
+    { name : "Cinnamon Coffee", cost : 140, id : 15  },
+    { name : "Espresso", cost :80, id : 16  },
+    { name : "Vannila Latte", cost : 140, id : 17  },
+    { name : "Filter Coffee", cost : 60, id : 18  },
 ]
 
 export const Snacks = [
     { name : "Sandwich", cost : 120, id : 21 },
-    { name : "Cottage Cheese Fry", cost : 240, id : 22 },
-    { name : "Garlic Bread", cost : 280, id : 23 },
-    { name : "Bread Sticks", cost : 120, id : 24 },
-    { name : "Veg Burger", cost : 240, id : 25 },
+    { name : "Cottage Cheese Fry", cost : 150, id : 22 },
+    { name : "Garlic Bread", cost : 140, id : 23 },
+    { name : "Bread Sticks", cost : 80, id : 24 },
+    { name : "Veg Burger", cost : 90, id : 25 },
     { name : "Veg Pizza", cost : 190, id : 26 },
 ]
 
 export const ColdCoffee = [
-    { name : "Vegan Shake", cost : 120, id : 31 },
-    { name : "Cold Coffee", cost : 240, id : 32 },
-    { name : "Cold Mocha", cost : 320, id : 33 },
-    { name : "Iced Tea", cost : 160, id : 34 },
-    { name : "Chilled Latte", cost : 120, id : 35 },
-    { name : "Chocolate Shake", cost : 190, id : 36 },
-    { name : "Belgian Chocolate", cost : 180, id : 37 },
+    { name : "Vegan Shake", cost : 90, id : 31 },
+    { name : "Cold Coffee", cost : 120, id : 32 },
+    { name : "Cold Mocha", cost : 150, id : 33 },
+    { name : "Iced Tea", cost : 80, id : 34 },
+    { name : "Chilled Latte", cost : 70, id : 35 },
+    { name : "Brownie Shake", cost : 190, id : 36 },
+    { name : "Belgian Chocolate", cost : 160, id : 37 },
 ]
 
 export const Dessert = [
-    { name : "Cheese Cake", cost : 120, id : 41 },
+    { name : "Cheese Cake", cost : 70, id : 41 },
     { name : "Choco Fantasy", cost : 120, id : 42 },
-    { name : "Brownie", cost : 120, id : 43 },
+    { name : "Brownie", cost : 60, id : 43 },
     { name : "Choco Fudge", cost : 120, id : 44 },
-    { name : "Vanilla Scoop", cost : 120, id : 45 },
-    { name : "Berry Cheesecake", cost : 120, id : 46 },
-    { name : "Strawberry Cake", cost : 120, id : 47 },
-    { name : "Red Velvet Cake", cost : 120, id : 48},
+    { name : "Vanilla Scoop", cost : 70, id : 45 },
+    { name : "Berry Cheesecake", cost : 110, id : 46 },
+    { name : "Strawberry Cake", cost : 60, id : 47 },
+    { name : "Red Velvet Cake", cost : 80, id : 48},
 
 ]
